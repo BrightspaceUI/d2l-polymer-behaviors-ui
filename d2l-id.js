13be16d8ffe0ee6a8c1dd9589b7fae36bdf88046
Dave Batiste <dave.batiste@d2l.com>
2019-08-09 22:33:23 -0400

    Use core for getUniqueId.

--- d2l-id.js
+++ d2l-id.js
@@ -1,14 +1,8 @@
+import { getUniqueId } from '@brightspace-ui/core/helpers/uniqueId.js';
+
 var Id = {
 
-	getUniqueId: function() {
-
-		if (window.D2L.Id._unique_index === undefined) {
-			window.D2L.Id._unique_index = 0;
-		}
-		window.D2L.Id._unique_index++;
-		return 'd2l-uid-' + window.D2L.Id._unique_index;
-
-	}
+	getUniqueId: getUniqueId
 
 };
 
