38267bd64b2c9c8851ec9e845b5522992a2f4a63
Dave Lockhart <dave.lockhart@d2l.com>
2020-08-06 14:41:49 -0400

    Merge pull request #61 from Brightspace/fix-dom [increment patch]

--- d2l-dom.js
+++ d2l-dom.js
@@ -6,53 +6,44 @@ import {
 } from '@brightspace-ui/core/helpers/dom.js';
 
 window.D2L = window.D2L || {};
+window.D2L.Dom = window.D2L.Dom || {};
+window.D2L.Dom.findComposedAncestor = findComposedAncestor;
+window.D2L.Dom.getComposedChildren = getComposedChildren;
+window.D2L.Dom.getComposedParent = getComposedParent;
+window.D2L.Dom.isComposedAncestor = isComposedAncestor;
+window.D2L.Dom.getOffsetParent = function(node) {
+	if (!window.ShadowRoot) {
+		return node.offsetParent;
+	}
 
-/** @polymerBehavior */
-D2L.Dom = {
-
-	findComposedAncestor: findComposedAncestor,
-
-	getComposedChildren: getComposedChildren,
-
-	getComposedParent: getComposedParent,
-
-	isComposedAncestor: isComposedAncestor,
-
-	getOffsetParent: function(node) {
-		if (!window.ShadowRoot) {
-			return node.offsetParent;
-		}
-
-		if (
-			!this.getComposedParent(node) ||
-			node.tagName === 'BODY' ||
-			window.getComputedStyle(node).position === 'fixed'
-		) {
-			return null;
-		}
-
-		let currentNode = this.getComposedParent(node);
-		while (currentNode) {
-			if (currentNode instanceof ShadowRoot) {
-				currentNode = this.getComposedParent(currentNode);
-			} else if (currentNode instanceof DocumentFragment) {
-				return null;
-			} else if (currentNode.tagName === 'BODY') {
-				return currentNode;
-			}
-			const position = window.getComputedStyle(currentNode).position;
-			const tagName = currentNode.tagName;
-
-			if (
-				(position && position !== 'static') ||
-				position === 'static' && (tagName === 'TD' || tagName === 'TH' || tagName === 'TABLE')
-			) {
-				return currentNode;
-			}
-			currentNode = this.getComposedParent(currentNode);
-		}
-
+	if (
+		!this.getComposedParent(node) ||
+		node.tagName === 'BODY' ||
+		window.getComputedStyle(node).position === 'fixed'
+	) {
 		return null;
 	}
 
+	let currentNode = this.getComposedParent(node);
+	while (currentNode) {
+		if (currentNode instanceof ShadowRoot) {
+			currentNode = this.getComposedParent(currentNode);
+		} else if (currentNode instanceof DocumentFragment) {
+			return null;
+		} else if (currentNode.tagName === 'BODY') {
+			return currentNode;
+		}
+		const position = window.getComputedStyle(currentNode).position;
+		const tagName = currentNode.tagName;
+
+		if (
+			(position && position !== 'static') ||
+			position === 'static' && (tagName === 'TD' || tagName === 'TH' || tagName === 'TABLE')
+		) {
+			return currentNode;
+		}
+		currentNode = this.getComposedParent(currentNode);
+	}
+
+	return null;
 };
